5a71a23ca3d76a43ca33460e05a9cb0520e9a769
OLGA <serikova-olga@mail.ru>
2022-07-12 15:50:33 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+Console.WriteLine("Сколько чисел Вы хотите ввести?");
+int numberM = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] numbers = new int[numberM];
+for (int i = 0; i < numberM; i++)
+{
+    Console.WriteLine("Введите число " + (i+1));
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (numbers[i] > 0) count++;
+}
+Console.WriteLine();
+Console.WriteLine("Введено чисел больше нуля: " + count);
